5c1952b67e1a71a2e42c751f2001ed6e1fd8d25e
Mohamed Hamed <mh3128619@gmail.com>
2026-07-08 18:40:10 +0300

    Improve formatting and readability of number sorting
    Refactor code for better readability and formatting.

--- Even-odd-number-sorter.py
+++ Even-odd-number-sorter.py
@@ -1,14 +1,15 @@
-numbers=(45,12,89,3,24,56,77,10,5)
-even_numbers=[]
-odd_numbers=[]
+numbers = (45, 12, 89, 3, 24, 56, 77, 10, 5)
+even_numbers = []
+odd_numbers = []
 
 for i in numbers:
-  if i%2==0:
-    even_numbers.append(i)
-  else:
-    odd_numbers.append(i)
+    if i % 2 == 0:
+        even_numbers.append(i)
+    else:
+        odd_numbers.append(i)
 
-  even_numbers.sort()
-  odd_numbers.sort()
-print(even_numbers)
-print(odd_numbers)
+even_numbers.sort()
+odd_numbers.sort()
+
+print("Even numbers:", even_numbers)
+print("Odd numbers:", odd_numbers)
