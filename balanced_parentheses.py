0d817141af5af0d4ee5961723ebf67bd1a6c5697
Mohamed Hamed <mh3128619@gmail.com>
2026-07-15 18:08:43 +0300

    Implement balanced parentheses checker

---
+++ balanced_parentheses.py
@@ -0,0 +1,16 @@
+def is_balanced(expression):
+    stack = []
+    
+    for char in expression:
+        if char == '(':
+            stack.append(char)
+        elif char == ')':
+            if not stack:
+                return False
+            stack.pop()
+
+    return len(stack) == 0
+
+print(is_balanced("(())"))
+print(is_balanced("(()"))
+print(is_balanced("())("))
